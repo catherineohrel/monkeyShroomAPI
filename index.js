509c6a3cee4443d76323e82659841565a63b5af9
catherineohrel <catherine_ohrel@hotmail.com>
2023-06-21 11:10:10 +0200

    creation branch dev and added img

--- index.js
+++ index.js
@@ -10,70 +10,70 @@ const data = [
         "name": "Bananes",
         "namePrix":"Bananes - 25 mushs",
         "prix": 25,
-        "image": "assets/bananes1.png"
+        "image": "./src/assets/bananes1.png"
     },
     {
         "id": 2,
         "name": "Chou Kale",
         "namePrix":"Chou Kale - 25 mushs",
         "prix": 25,
-        "image": "assets/chouxKale1.png"
+        "image": "./src/assets/chouxKale1.png"
     },
     {
         "id": 3,
         "name": "Poupee Gonflable",
         "namePrix":"Poupee Gonflable - 100 mushs",
         "prix": 100,
-        "image": "assets/poupee1.png"
+        "image": "./src/assets/poupee1.png"
     },
     {
         "id": 4,
         "name": "Anti Puces",
         "namePrix":"Anti Puces - 75 mushs",
         "prix": 75,
-        "image": "assets/antiPuces1.png"
+        "image": "./src/assets/antiPuces1.png"
     },
     {
         "id": 5,
         "name": "Kit Reparation",
         "namePrix":"Kit Reparation - 150 mushs",
         "prix": 150,
-        "image": "assets/kitLianes1.png"
+        "image": "./src/assets/kitLianes1.png"
     },
     {
         "id": 6,
         "name": "Gratte Dos",
         "namePrix":"Gratte Dos - 50 mushs",
         "prix": 50,
-        "image": "assets/gratteDos1.png"
+        "image": "./src/assets/gratteDos1.png"
     },
     {
         "id": 7,
         "name": "Slip Surprise",
         "namePrix":"Slip Surprise - 50 mushs",
         "prix": 50,
-        "image": "assets/slips1.png"
+        "image": "./src/assets/slips1.png"
     },
     {
         "id": 8,
         "name": "Vies",
         "namePrix":"Vies - 100 mushs",
         "prix": 100,
-        "image": "assets/Vies1.png"
+        "image": "./src/assets/Vies1.png"
     },
     {
         "id": 9,
         "name": "Surprise",
         "namePrix":"Surprise - 75 mushs",
         "prix": 75,
-        "image": "assets/pochetteSurprise1.png"
+        "image": "./src/assets/pochetteSurprise1.png"
     },
     {
         "id": 10,
         "name": "Champis",
         "namePrix":"Champis - 200 mushs",
         "prix": 200,
-        "image": "assets/mushroom1.png"
+        "image": "./src/assets/mushroom1.png"
     }
 ]
 
